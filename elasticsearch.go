b77ba96dc4296ae4d27501616067f58c5185c4eb
SebastianGao <53056387+Sebastiangao0219@users.noreply.github.com>
2021-07-06 02:07:01 -0500

    First commit

---
+++ elasticsearch.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+
+	"github.com/olivere/elastic/v7"
+)
+
+const (
+	ES_URL      = "http://10.128.0.2:9200"
+	ES_PASSWORD = "Gao19890219"
+)
+
+func readFromES(query elastic.Query, index string) (*elastic.SearchResult, error) {
+	client, err := elastic.NewClient(
+		elastic.SetURL(ES_URL),
+		elastic.SetBasicAuth("elastic", ES_PASSWORD))
+	if err != nil {
+		return nil, err
+	}
+
+	searchResult, err := client.Search().
+		Index(index).
+		Query(query).
+		Pretty(true).
+		Do(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return searchResult, nil
+}
+
+func saveToES(i interface{}, index string, id string) error {
+	client, err := elastic.NewClient(
+		elastic.SetURL(ES_URL),
+		elastic.SetBasicAuth("elastic", ES_PASSWORD))
+	if err != nil {
+		return err
+	}
+
+	_, err = client.Index().
+		Index(index).
+		Id(id).
+		BodyJson(i).
+		Do(context.Background())
+	return err
+}
